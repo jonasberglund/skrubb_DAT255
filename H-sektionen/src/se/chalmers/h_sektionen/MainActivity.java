289fa8f96a0bd47cb1624f735ad0d6ab36d611f3
roffin <robin.tornquist@gmail.com>
2013-09-18 14:58:52 +0200

    Added slide menu

--- H-sektionen/src/se/chalmers/h_sektionen/MainActivity.java
+++ H-sektionen/src/se/chalmers/h_sektionen/MainActivity.java
@@ -1,17 +1,84 @@
 package se.chalmers.h_sektionen;
 
+import se.chalmers.h_sektionen.utils.MenuItems;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.FrameLayout;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+    private String[] menuTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private FrameLayout frameLayout;
+	
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        menuTitles = getResources().getStringArray(R.array.menu_titles);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        
+        frameLayout = (FrameLayout) findViewById(R.id.content_frame);
+        
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, menuTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+		
+		
 	}
-
+	
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+//            Intent intent = new Intent(parent.getContext(), LunchActivity.class);
+//        	startActivity(intent);
+        	frameLayout.removeAllViews();
+        	LayoutInflater inflater = getLayoutInflater();
+        	
+        	switch (position) {
+        	case MenuItems.NEWS:
+        		frameLayout.addView(inflater.inflate(R.layout.activity_main, null));
+        		break;
+        	case MenuItems.LUNCH:
+        		frameLayout.addView(inflater.inflate(R.layout.view_lunch, null));
+        		break;
+        	case MenuItems.PUB:
+        		frameLayout.addView(inflater.inflate(R.layout.view_pub, null));
+        		break;
+        	case MenuItems.INFO:
+        		frameLayout.addView(inflater.inflate(R.layout.view_info, null));
+        		break;
+        	case MenuItems.EVENTS:
+        		frameLayout.addView(inflater.inflate(R.layout.view_events, null));
+        		break;
+        	case MenuItems.VOTE:
+        		frameLayout.addView(inflater.inflate(R.layout.view_vote, null));
+        		break;
+        	case MenuItems.SUGGEST:
+        		frameLayout.addView(inflater.inflate(R.layout.view_suggest, null));
+        		break;	
+        	default:
+        		return;
+        	}
+        	
+        	mDrawerLayout.closeDrawer(Gravity.LEFT);
+        }
+    }
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.

d49643f98ed5da812b6ce353a16a12395e97332d
OskarG <oskar_erik_gustavsson@hotmail.com>
2013-10-04 10:40:18 +0200

    Cleaned up logic and added comments

--- H-sektionen/src/se/chalmers/h_sektionen/SuggestActivity.java
+++ H-sektionen/src/se/chalmers/h_sektionen/SuggestActivity.java
@@ -13,33 +13,36 @@ public class SuggestActivity extends BaseActivity {
 	protected void onResume() {
 
 		setCurrentView(MenuItems.SUGGEST);
-		createLunchView();
+		createView();
 		
 		super.onResume();
 	}
 	
-	private void createLunchView(){
+	private void createView(){
 		getFrameLayout().removeAllViews();
 		getFrameLayout().addView(getLayoutInflater().inflate(R.layout.view_suggest, null));
     	
     }
 	
+	/**
+	 * Starts an e-mail client and fill in all fields with text from the view and strings.
+	 * @param view the view to fetch the message text from.
+	 */
 	public void sendSuggestEmail(View view){
 		Intent intent = new Intent(Intent.ACTION_SENDTO);
 		intent.setType("text/plain");
 		
-		EditText editText = (EditText) findViewById(R.id.suggest_edit_message);	//fetch the text from the user
-		String message = editText.getText().toString();							//...and make it into a string
+		EditText editText = (EditText) findViewById(R.id.suggest_edit_message);	//fetch the text from the view
+		String message = editText.getText().toString();							
 		
-		intent.putExtra(Intent.EXTRA_SUBJECT, "Förslag till H-sektionen");		//the subject for the email
-		intent.putExtra(Intent.EXTRA_TEXT, message); 							//Adds the text from the editText field.
-		intent.setData(Uri.parse("mailto:"+getString(R.string.suggest_emailReceiver))); //Sets the receiver of the email
-		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); 						
+		intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.suggest_subject));		
+		intent.putExtra(Intent.EXTRA_TEXT, message); 							
+		intent.setData(Uri.parse("mailto:"+getString(R.string.suggest_emailReceiver))); //Sets the receiver of the email						
 		
 		try{
-			startActivity(intent);	//try to start the intent, if there are any email clients on the phone.
-		}catch (android.content.ActivityNotFoundException ex) { //if not, post a toast to let the user know.
-			Toast.makeText(SuggestActivity.this, "Det verkar inte finnas någon E-mail applikation på din telefon.", Toast.LENGTH_SHORT).show();
+			startActivity(intent);	
+			}catch (android.content.ActivityNotFoundException ex) { //if no E-mail client is found, post a toast to let the user know.
+			Toast.makeText(SuggestActivity.this,getString(R.string.suggest_noEmailClientFound), Toast.LENGTH_SHORT).show();
 		}
 	}
 
